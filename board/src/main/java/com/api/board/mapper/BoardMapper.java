79991a7ff30c67861af6880b4ce243fe46e1090b
yhjeong84 <ajou.jyh@gmail.com>
2021-04-15 15:29:17 +0900

    set Mapper and test

--- board/src/main/java/com/api/board/mapper/BoardMapper.java
+++ board/src/main/java/com/api/board/mapper/BoardMapper.java
@@ -1,11 +1,11 @@
 package com.api.board.mapper;
-
+ 
 import java.util.List;
-
+ 
 import com.api.board.domain.Board;
-
+ 
 public interface BoardMapper {
-    
+ 
     /**
      * 게시글 목록 조회
      * 
@@ -13,4 +13,40 @@ public interface BoardMapper {
      * @throws Exception
      */
     public List<Board> getBoardList() throws Exception;
+    
+    /**
+     * 게시글 상세 조회
+     * 
+     * @param board_seq
+     * @return
+     * @throws Exception
+     */
+    public Board getBoardDetail(int board_seq) throws Exception;
+    
+    /**
+     * 게시글 등록 
+     * 
+     * @param board
+     * @return
+     * @throws Exception
+     */
+    public int insertBoard(Board board) throws Exception;  
+    
+    /**
+     * 게시글 수정 
+     * 
+     * @param board
+     * @return
+     * @throws Exception
+     */
+    public int updateBoard(Board board) throws Exception;
+    
+    /**
+     * 게시글 삭제 
+     * 
+     * @param board_seq
+     * @return
+     * @throws Exception
+     */
+    public int deleteBoard(int board_seq) throws Exception;
 }
