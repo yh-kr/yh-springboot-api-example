85e7f6cc6a1ac215f7ed5ab92f51c64376ee989a
yhjeong84 <ajou.jyh@gmail.com>
2021-04-15 16:49:25 +0900

    set Controller and test

--- board/src/main/java/com/api/board/config/WebMvcConfig.java
+++ board/src/main/java/com/api/board/config/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package com.api.board.config;
  
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
 import com.api.board.interceptor.BoardInterceptor;
  
+@ComponentScan(basePackages = { "com.api.board.controller" }, useDefaultFilters = false, includeFilters = { @Filter(Controller.class) })
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
  
