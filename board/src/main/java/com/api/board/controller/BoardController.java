0c4bd8e76effdd470da59a50d905900d11c31089
yhjeong84 <ajou.jyh@gmail.com>
2021-04-15 14:17:12 +0900

    set Logback

--- board/src/main/java/com/api/board/controller/BoardController.java
+++ board/src/main/java/com/api/board/controller/BoardController.java
@@ -1,20 +1,24 @@
 package com.api.board.controller;
-
+ 
 import java.util.List;
-
-import com.api.board.domain.Board;
-import com.api.board.service.BoardService;
-
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-@RequestMapping(value="/board")
+ 
+import com.api.board.domain.Board;
+import com.api.board.service.BoardService;
+ 
+@RequestMapping(value = "/board")
 @Controller
 public class BoardController {
-    
+ 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+ 
     @Autowired
     private BoardService boardService;
  
@@ -22,6 +26,14 @@ public class BoardController {
     @ResponseBody
     public List<Board> getBoardList() throws Exception {
  
+        logger.info("===== ===== ===== logging.level.com.api.board.controller - Logger Level ===== ===== =====");
+        logger.trace("Logger Level - [TRACE]"); 
+        logger.debug("Logger Level - [DEBUG]"); 
+        logger.info("Logger Level - [INFO]"); 
+        logger.warn("Logger Level - [WARN]"); 
+        logger.error("Logger Level - [ERROR]");
+        logger.info("===== ===== ===== logging.level.com.api.board.controller - Logger Level ===== ===== =====");
+        
         List<Board> boardList = boardService.getBoardList();
  
         return boardList;
--- board/src/main/java/com/api/board/service/BoardService.java
+++ board/src/main/java/com/api/board/service/BoardService.java
@@ -1,16 +1,20 @@
 package com.api.board.service;
-
+ 
 import java.util.List;
-
-import com.api.board.domain.Board;
-import com.api.board.mapper.BoardMapper;
-
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
+import com.api.board.domain.Board;
+import com.api.board.mapper.BoardMapper;
+ 
 @Service
 public class BoardService {
     
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+ 
     @Autowired
     private BoardMapper boardMapper;
  
@@ -21,6 +25,15 @@ public class BoardService {
      * @throws Exception
      */
     public List<Board> getBoardList() throws Exception {
+        
+        logger.info("===== ===== ===== logging.level.com.api.board - Logger Level ===== ===== =====");
+        logger.trace("Logger Level - [TRACE]"); 
+        logger.debug("Logger Level - [DEBUG]"); 
+        logger.info("Logger Level - [INFO]"); 
+        logger.warn("Logger Level - [WARN]"); 
+        logger.error("Logger Level - [ERROR]");
+        logger.info("===== ===== ===== logging.level.com.api.board - Logger Level ===== ===== =====");
+        
         return boardMapper.getBoardList();
     }
 }
